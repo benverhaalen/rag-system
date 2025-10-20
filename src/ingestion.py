270fb455e93d9f77c1f210ec181d177fa55e5849
Ben Verhaalen <benverhaalen@Bens-MacBook-Pro.local>
2025-10-20 12:05:32 -0500

    Add text chunking

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,12 +1,7 @@
-# get link
-
-# save file as a csv into data/docs
 from youtube_transcript_api import YouTubeTranscriptApi, FetchedTranscript, FetchedTranscriptSnippet
 from typing import Any, Dict, List, Optional
-from dataclasses import dataclass
-from pathlib import Path
 import re
-import csv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 urlTest = "https://www.youtube.com/watch?v=jyLXcy5SGd8"
 youtube_id_re = re.compile(r"(?:(?<=v=)|(?<=be/)|(?<=embed/)|(?<=v/)|(?<=V/)|(?<=\?v=)|(?<=&v=))(?P<id>[\w-]{11})")
@@ -25,6 +20,62 @@ def get_transcript(url: str) -> List[Dict]:
         
     return fetched_transcript.to_raw_data()
     
+# chunk youtube transcript using langchain
+def chunk_transcript(transcript: List) -> List:
+    # creating a full text str of the transcript and a list to map the text to timestamps
+    full_text = ""
+    position_timestamp_map = []
+    
+    # creating the map
+    for segment in transcript:
+        start_pos = len(full_text)
+        segment_text = segment['text']
+        
+        if full_text and not full_text.endswith(' '):
+            full_text += ' '
+            start_pos += 1
+        
+        full_text += segment_text
+        end_pos = len(full_text)
+        
+        position_timestamp_map.append({
+            'char_start': start_pos,
+            'char_end': end_pos,
+            'timestamp': segment['start'],
+            'text': segment_text
+        })
+        
+    # splitting full text
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 800,
+        chunk_overlap=200,
+        separators=["\n\n", "\n", ". ", " ", ""]
+    )
+    chunks = text_splitter.split_text(full_text)
+        
+    # creating list of the split chunks mapped to the timestamp they appear
+    chunks_with_timestamps = []
+    
+    for chunk in chunks:
+        chunk_start = full_text.find(chunk)
+        
+        if chunk_start == -1:
+            print("Could not find chunk")
+            
+        timestamp = find_timestamp(chunk_start, position_timestamp_map)
+        
+        chunks_with_timestamps.append({
+            'text': chunk,
+            'timestamp': timestamp
+        })
+        
+    return chunks_with_timestamps
 
-ft = get_transcript(urlTest)
-print(ft[:3])
+# given a char position, output the original transcript segment it belongs to
+def find_timestamp(char_pos: int, positions: list) -> float:
+    for segment in positions:
+        if segment['char_start'] <= char_pos < segment['char_end']:
+            return segment['timestamp']
+        
+    # return last known timestamp if not found
+    return positions[-1]['timestamp'] if positions else 0.0
